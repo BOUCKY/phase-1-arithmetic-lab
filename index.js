f790af417d1c8a3311fc4bf9951315a92693e38e
Alexis <alexisbou16@gmail.com>
2023-05-22 13:41:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 67;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
